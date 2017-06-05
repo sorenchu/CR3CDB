4abf38edb8813fb3a73eb42faf5f5a42656e0c12
sorenchu <antonioostras@gmail.com>
2017-06-05 21:04:10 +0200

    Allowing to import playerData

--- src/DatabaseBundle/Controller/Import/playerData.py
+++ src/DatabaseBundle/Controller/Import/playerData.py
@@ -34,28 +34,61 @@ def deleteFile(pathToFile):
   print pathToFile 
   os.remove(pathToFile)
 
-def getSex(string):
-  isMale = 'Masculino'
-  patternSex = re.compile(isMale)
-  if patternSex.search(string):
-    return 'male'
-  return 'female'
-
-def getCategory(string):
+def getPlayerOrCoach(string):
   isSportman = 'Deportista'
   patternSport = re.compile(isSportman)
   if patternSport.search(string):
     return 'player'
   return 'coach'
 
-def getSql(string):
+def getId(arrayForQuery):
+  query = 'SELECT id FROM personalData WHERE name = \"' + arrayForQuery[3] + '\" AND surname = \"' + arrayForQuery[4] + '\";'
+  connection = openDatabase()
+  cursor = connection.cursor()
+  cursor.execute(query)
+  result = cursor.fetchall()[0][0]
+  return result
+
+def alterPersonalData(string):
   splitting = ','
   arrayForQuery = string.split(splitting)
-  if 'player' == getCategory(string):
-    query = 'INSERT INTO personalData(name, surname, sex, is_player, is_coach, is_parent, is_member) '
+  personalDataId = getId(arrayForQuery)
+  if 'player' == getPlayerOrCoach(string):
+    query = 'UPDATE personalData SET is_player'
   else:
-    query = 'INSERT INTO personalData(name, surname, sex, is_coach, is_player, is_parent, is_member) '
-  query += 'VALUES(\"' + arrayForQuery[3] + '\", \"' + arrayForQuery[4] + '\", \"' + getSex(string) + '\", 1, 0, 0, 0);\n'
+    query = 'UPDATE personalData SET is_coach'
+  query += '=1 WHERE id = ' + str(personalDataId) + ';\n'
+  return query
+
+def getCategory(string):
+  if 'Sub 21' == string:
+    return 'senior'
+  elif 'Sub 18' == string:
+    return 'cadete'
+  else: 
+    return 'senior'
+
+def getDefaultSeason():
+  query = 'SELECT id FROM season WHERE defaultseason = 1;'
+  connection = openDatabase()
+  cursor = connection.cursor()
+  cursor.execute(query)
+  result = cursor.fetchall()[0][0]
+  return str(result)
+
+
+# TODO: it is needed to include an option for updating tables instead inserting
+def insertIntoPlayerOrCoachData(string):
+  splitting = ','
+  arrayForQuery = string.split(splitting)
+  personalDataId = getId(arrayForQuery)
+  defaultSeason = getDefaultSeason()
+  if 'player' == getPlayerOrCoach(string):
+    query = 'INSERT INTO playerData(category, number, personalData_id, season_id) '
+    query += 'VALUES(\"' + getCategory(arrayForQuery[7]) + '\", ' + arrayForQuery[0] + ', ' + str(personalDataId) + ', ' + defaultSeason + ');\n'
+  else:
+    query = 'INSERT INTO coachData(category, number, personalData_id) '
+    query += 'VALUES(' + 'senior' + ', ' + arrayForQuery[0] + ', ' + str(personalDataId) + ');\n'
   return query
 
 def parsingFile(source, destiny):
@@ -63,7 +96,9 @@ def parsingFile(source, destiny):
   pattern = re.compile(frmIdNumber)
   for line in source:
     if pattern.search(line):
-      sqlQuery = getSql(line)
+      sqlQuery = alterPersonalData(line)
+      putIntoFile(destiny, sqlQuery)
+      sqlQuery = insertIntoPlayerOrCoachData(line)
       putIntoFile(destiny, sqlQuery)
   return 1 
 
@@ -86,7 +121,7 @@ def main():
   pathOfFileToParse = sys.argv[1]
   fileToParse = readFile(pathOfFileToParse)
   fileGenerated = writeFile();
-  if (parsingFile(fileToParse, fileGenerated)):
+  if parsingFile(fileToParse, fileGenerated):
     pathOfFileGenerated = os.getcwd() + '/' + fileGenerated.name
     closeFile(fileGenerated)
     fileGenerated = readFile(pathOfFileGenerated)
