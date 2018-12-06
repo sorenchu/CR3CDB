f945b1a984f74290ac88096db425e9f7c0baffee
sorenchu <antonioostras@gmail.com>
2018-12-06 13:28:17 +0100

    Avoiding that null can be set as season

--- src/DatabaseBundle/Form/Season/SeasonType.php
+++ src/DatabaseBundle/Form/Season/SeasonType.php
@@ -31,7 +31,7 @@ class SeasonType extends AbstractType
               return $er->createQueryBuilder('season');
             },
             'choices' => $this->season,
-            'required' => false,
+            'required' => true,
             'multiple' => false,
             'expanded' => false,
             )
