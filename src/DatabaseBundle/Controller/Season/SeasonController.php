cb666498bbcff4f7eb0e34951803b12039889fe2
sorenchu <antonioostras@gmail.com>
2020-05-03 11:25:13 +0200

    adding path to parameters in season controller

--- src/DatabaseBundle/Controller/Season/SeasonController.php
+++ src/DatabaseBundle/Controller/Season/SeasonController.php
@@ -12,13 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SeasonController extends Controller {
 
-    function handleForm(int $id, Season $season, Request $request) {
+    function handleForm(
+        int $id,
+        string $path,
+        Season $season,
+        Request $request
+    ) {
         $seasonForm = $this->createForm(SeasonType::class, $season);
         $seasonForm->handleRequest($request);
         $seasonId = $seasonForm->get('season')->getData()->getId();
         if ($seasonForm->isSubmitted()) {
             return $this->redirectToRoute(
-                    'edit_person',
+                    $path,
                     array(
                         'id' => $id,
                         'seasonId' => $seasonId
